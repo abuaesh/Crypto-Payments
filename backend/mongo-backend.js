26fe02740c2604f1cd0d658fcd52b0e9e956d8b7
abuaesh <noha.abuaesh@gmail.com>
2020-08-01 01:25:25 +0300

    Working on the mongodb code

--- backend/mongo-backend.js
+++ backend/mongo-backend.js
@@ -1,13 +1,13 @@
+// Reference: https://www.mongodb.com/blog/post/quick-start-nodejs-mongodb--how-to-get-connected-to-your-database
 //import customers from './customers.json';
-import MongoDB from 'mongodb';
+import MongoClient from 'mongodb';
 
 export default class MongoBackend{
     constructor(uri){
         //Configure mongoDB variables
-        let MongoClient = MongoDB.MongoClient;
-        let client = new MongoClient(uri, { useNewUrlParser: true });
+        let client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
         //Connect to mongoDB cloud
-        client.connect(err => {
+        await client.connect(err => {
             const collection = client.db("krakenChallenge").collection("customers");
             // perform actions on the collection object
             console.log("Customers collection created!");
