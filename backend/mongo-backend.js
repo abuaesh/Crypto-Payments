26c723785acc8a55a3130a73b50f8bbb24dd6185
abuaesh <noha.abuaesh@gmail.com>
2020-08-02 01:14:07 +0300

    Connected to mongodb cloud cluster

--- backend/mongo-backend.js
+++ backend/mongo-backend.js
@@ -1,22 +1,71 @@
 // Reference: https://www.mongodb.com/blog/post/quick-start-nodejs-mongodb--how-to-get-connected-to-your-database
 //import customers from './customers.json';
-import client from 'mongodb';
+import mongodb from 'mongodb';
 
 export default class MongoBackend{
     constructor(uri){
+
+        const MongoClient = mongodb.MongoClient;
+
+//const url = "mongodb://localhost:27017/";
+const client = new MongoClient(uri, { useUnifiedTopology: true }); // useUnifiedTopology removes a warning
+
+// Connect
+client
+  .connect()
+  .then(client =>
+    client
+      .db()
+      .admin()
+      .listDatabases() // Returns a promise that will resolve to the list of databases
+  )
+  .then(dbs => {
+    console.log("Mongo databases", dbs);
+  })
+  .finally(() => client.close()); // Closing after getting the data
+
+
+
+
+
+
+
+
+
+
+
+        /*
         //Create mongoDB client
         //this.client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
+        let client = mongo.MongoClient;
+        let Db = mongo.Db;
+        let Server = mongo.Server;
         try{
             //Connect to mongoDB cluster
-            client.connect(uri, function(err,db){
+            client.connect(uri, function(err, db){
                 if (err) throw err;
-                console.log('Database created!\n'+db);
-                db.close();
+                
+                var db = new Db('test', new Server('localhost', 27017));
+                // Establish connection to db
+                db.open(function(err, db) {
+
+                // Use the admin database for the operation
+                var adminDb = db.admin();
+
+                // List all the available databases
+                adminDb.listDatabases(function(err, dbs) {
+                    assert.equal(null, err);
+                    assert.ok(dbs.databases.length > 0);
+
+                    db.close();
+                });
+                });
+
             });// end client.connect
         }catch (e) {
             console.error(e);
         } //end try
+        */
         
     } //end constructor
 
--- src/app.js
+++ src/app.js
@@ -7,25 +7,22 @@ import Dotenv from 'dotenv';
 //import Transactions2 from './transactions-2.json';
 
 (async() => {
+    //import environment variables
     const result = Dotenv.config()
-    
-    if (result.error) {
-    throw result.error
-    }
-    
-    console.log(result.parsed)
+    if (result.error) throw result.error
 
+    var dbConfig = null;//holds the connection string to the db to be used
+
+    //Select DB Type
     const backendType = 0; //0 for mongoDB(default),
                             // 1 for SQL. 
-
-    var dbConfig = null;
 
     if(!backendType) // mongoDB selected
     {
         dbConfig = process.env.MONGO_CONNECTION_STRING;
         console.log('uri:'+dbConfig);
     }
-    // Uncomment this block if your SQL backend is already configured, otherwise defaults will be used to create a new  sql DB
+    /* Uncomment this block if your SQL backend is already configured, otherwise defaults will be used to create a new  sql DB
     else //sql selected
     {
         dbConfig = {
@@ -35,8 +32,7 @@ import Dotenv from 'dotenv';
             database: process.env.SQL_DB
         };
     } //end backend type selection
-    
-    
+    */    
 
     let backend = (!backendType)? new MongoBackend(dbConfig): new SqlBackend(dbConfig);
 
